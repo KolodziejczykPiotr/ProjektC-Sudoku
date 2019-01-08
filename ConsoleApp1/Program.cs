1a9953d55b6c4f50671c8ee961203b24dd7e7bcc
KolodziejczykPiotr <piotr.kolodziejczyk@microsoft.wsei.edu.pl>
2019-01-08 21:58:56 +0100

    Add Engine

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SudokuLibrary;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+             SudokuEngine.NewGame(ref SudokuEngine.Board);
+             SudokuEngine.DrawBoard(ref SudokuEngine.Board, out SudokuEngine.s);
+
+           
+            Console.ReadLine();
+        }
+
+       
+
+        
+
+
+    }
+}
---
+++ SudokuLibrary/SudokuEngine.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuLibrary
+{
+    public static class SudokuEngine
+    {
+        public static int[,] Board = new int[9, 9];
+        public static string s;
+
+        public static void NewGame(ref int[,] Board)
+        {
+            for(int i = 0; i < 9; i++)
+            {
+                for(int x = 0; x < 9; x++)
+                {
+                    //Board[i, x] = (i * 3 + i / 3 + x) % 9 + 1;
+                }
+            }
+        }
+                 
+        public static void DrawBoard(ref int[,] b , out string _s)
+        {
+            for(int x = 0; x < 9; x++)
+            {
+                for(int y = 0; y < 9; y++)
+                {
+                    s += b[x, y].ToString()+" " ;
+                }
+                s += Environment.NewLine;
+            }
+            
+            Console.WriteLine(s);
+            _s = s;
+            s = "";
+            
+           
+        }
+
+                    
+                
+             
+
+
+     
+
+
+
+
+
+
+    }
+}
