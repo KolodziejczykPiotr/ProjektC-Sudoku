4cfd90ceb727190928f63877851c46d442da7468
Cebul23 <piotr.kolodziejczyk@microsoft.wsei.edu.pl>
2019-01-31 22:12:12 +0100

    End Game feature

--- sudoku/MainWindow.xaml.cs
+++ sudoku/MainWindow.xaml.cs
@@ -17,28 +17,28 @@ namespace sudoku
 {
     public partial class MainWindow : Window
     {
-        public int[,] SudokuBoard = new int[9, 9];
+        int[,] SudokuBoard = new int[9, 9];
         UIElement[] test = new UIElement[90];
         Border[] borders = new Border[90];
-        TextBox[,] texts = new TextBox[9,9];
+        TextBox[,] texts = new TextBox[9, 9];
         int[] Array = new int[9];
         bool flag = false;
         public MainWindow()
         {
             InitializeComponent();
-            
-          
+
+
         }
 
 
-       
+
 
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             SudokuGrid.Children.CopyTo(test, 0);
 
-            SudokuEngine.NewGame(SudokuBoard,Array);
+            SudokuEngine.NewGame(SudokuBoard, Array);
 
             for (int i = 0; i < borders.Length; i++)
             {
@@ -63,21 +63,21 @@ namespace sudoku
                     }
                     z++;
                 }
-                
+
             }
-            
+
             for (int i = 0; i < 9; i++)
             {
                 for (int j = 0; j < 9; j++)
                 {
-                    
-                    if(texts[i,j]!=null)
-                    texts[i,j].Text = SudokuBoard[i, j].ToString();
-                    if (texts[i,j].Text == "0")
+
+                    if (texts[i, j] != null)
+                        texts[i, j].Text = SudokuBoard[i, j].ToString();
+                    if (texts[i, j].Text == "0")
                     {
                         texts[i, j].Text = "";
                     }
-                        
+
                 }
 
 
@@ -85,11 +85,11 @@ namespace sudoku
             }
 
 
-            
-            foreach(var i in texts)
+
+            foreach (var i in texts)
             {
-                if(i!=null)
-                i.TextChanged += CheckAfter;
+                if (i != null)
+                    i.TextChanged += CheckAfter;
             }
             for (int i = 0; i < 9; i++)
             {
@@ -99,98 +99,91 @@ namespace sudoku
                 }
             }
         }
-       
-        
-        public void CheckAfter(object o , RoutedEventArgs e)
+
+
+        public void CheckAfter(object o, RoutedEventArgs e)
         {
-            
+
             flag = false;
-           
+
             l = (TextBox)e.OriginalSource;
 
             string[] NameArray = new string[2];
 
-            NameArray =l.Name.Split('_');
+            NameArray = l.Name.Split('_');
 
-            if(texts[Convert.ToInt32(NameArray[1]), Convert.ToInt32(NameArray[2])]!=null)
-            texts[Convert.ToInt32(NameArray[1]), Convert.ToInt32(NameArray[2])].Text = l.Text;
-            
+            if (texts[Convert.ToInt32(NameArray[1]), Convert.ToInt32(NameArray[2])] != null)
+                texts[Convert.ToInt32(NameArray[1]), Convert.ToInt32(NameArray[2])].Text = l.Text;
+
 
             texts[Convert.ToInt32(NameArray[1]), Convert.ToInt32(NameArray[2])].Background = Brushes.White;
 
-            //Sprawdza Maly kwadrat
+            // Sprawdza Maly kwadrat
             if (!flag)
             {
-                int first = 0;
-                int second = 0;
-                switch (Convert.ToInt32(NameArray[1]))
-                {
-                    case 0:
-                    case 1:
-                    case 2:
-                        first = 0;
-                        break;
-                    case 3:
-                    case 4:
-                    case 5:
-                        first = 3;
-                        break;
-                    case 6:
-                    case 7:
-                    case 8:
-                        first = 6;
-                        break;
-
-
-
-                }
-                
-                switch (Convert.ToInt32(NameArray[2]))
-                {
-                    case 0:
-                    case 1:
-                    case 2:
-                        second = 0;
-                        break;
-                    case 3:
-                    case 4:
-                    case 5:
-                        second = 3;
-                        break;
-                    case 6:
-                    case 7:
-                    case 8:
-                        second = 6;
-                        break;
-                }
-                
-                int number = 0;
-                for (int r = first; r < first + 3; r++)
-                {
-                    for (int l = second; l < second + 3; l++)
-                    {
-                        if (texts[r, l].Text != "")
-                        {
-                            Array[number] = Convert.ToInt32(texts[r, l].Text);
-                        }
-                        if (texts[r, l].Text == "")
-                        {
-                            Array[number] = 0;
-                        }
-
-                        number++;
-
-                    }
-
-                }
+                CheckSmallSquare(NameArray);
             }
             flag = SudokuEngine.CheckAfterChangeNumbers(Array);
-           
+
             //Sprawdza w pionie
-            if(!flag)
+            if (!flag)
+                CheckVertical(NameArray);
+
+            flag = SudokuEngine.CheckAfterChangeNumbers(Array);
+            //Sprawdza w poziomie
+            if (!flag)
+            {
+                CheckHorizontal(NameArray);
+            }
+
+            flag = SudokuEngine.CheckAfterChangeNumbers(Array);
+
+            if (flag)
+            {
+                texts[Convert.ToInt32(NameArray[1]), Convert.ToInt32(NameArray[2])].Background = Brushes.Red;
+            }
+
             for (int i = 0; i < 9; i++)
             {
-                if (texts[Convert.ToInt32(NameArray[1]),i].Text != "")
+                for (int j = 0; j < 9; j++)
+                {
+                    if (texts[i, j].Text != "")
+                        SudokuBoard[i, j] = Convert.ToInt32(texts[i, j].Text);
+                    else
+                        SudokuBoard[i, j] = 0;
+                }
+            }
+            
+
+            if (SudokuEngine.EndGame(SudokuBoard))
+            {
+                EndGameLabel.Visibility = Visibility.Visible;
+            }
+
+
+        }
+
+        private void CheckHorizontal(string[] NameArray)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (texts[i, Convert.ToInt32(NameArray[2])].Text != "")
+                    Array[i] = Convert.ToInt32(texts[i, Convert.ToInt32(NameArray[2])].Text);
+                if (texts[i, Convert.ToInt32(NameArray[2])].Text == "")
+                {
+                    Array[i] = 0;
+                }
+
+
+
+            }
+        }
+
+        private void CheckVertical(string[] NameArray)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (texts[Convert.ToInt32(NameArray[1]), i].Text != "")
                     Array[i] = Convert.ToInt32(texts[Convert.ToInt32(NameArray[1]), i].Text);
                 if (texts[Convert.ToInt32(NameArray[1]), i].Text == "")
                 {
@@ -200,33 +193,175 @@ namespace sudoku
 
 
             }
-            flag = SudokuEngine.CheckAfterChangeNumbers(Array);
-            //Sprawdza w poziomie
-            if (!flag)
-            {
-                for (int i = 0; i < 9; i++)
-                {
-                    if (texts[i, Convert.ToInt32(NameArray[2])].Text != "")
-                        Array[i] = Convert.ToInt32(texts[i, Convert.ToInt32(NameArray[2])].Text);
-                    if (texts[i, Convert.ToInt32(NameArray[2])].Text == "")
-                    {
-                        Array[i] = 0;
-                    }
-
-
-
-                }
-            }
-            
-            flag = SudokuEngine.CheckAfterChangeNumbers(Array);
-            
-            if (flag)
-            {
-                texts[Convert.ToInt32(NameArray[1]), Convert.ToInt32(NameArray[2])].Background = Brushes.Red;
-            }
-
-
         }
 
+        private void CheckSmallSquare(string[] NameArray)
+        {
+            int first = 0;
+            int second = 0;
+            switch (Convert.ToInt32(NameArray[1]))
+            {
+                case 0:
+                case 1:
+                case 2:
+                    first = 0;
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    first = 3;
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    first = 6;
+                    break;
+
+
+
+            }
+
+            switch (Convert.ToInt32(NameArray[2]))
+            {
+                case 0:
+                case 1:
+                case 2:
+                    second = 0;
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    second = 3;
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    second = 6;
+                    break;
+            }
+
+            int number = 0;
+            for (int r = first; r < first + 3; r++)
+            {
+                for (int l = second; l < second + 3; l++)
+                {
+                    if (texts[r, l].Text != "")
+                    {
+                        Array[number] = Convert.ToInt32(texts[r, l].Text);
+                    }
+                    if (texts[r, l].Text == "")
+                    {
+                        Array[number] = 0;
+                    }
+
+                    number++;
+
+                }
+
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            texts[0, 0].Text = 4.ToString();
+            texts[0, 1].Text = 2.ToString();
+            texts[0, 2].Text = 8.ToString();
+            texts[0, 3].Text = 3.ToString();
+            texts[0, 4].Text = 5.ToString();
+            texts[0, 5].Text = 1.ToString();
+            texts[0, 6].Text = 6.ToString();
+            texts[0, 7].Text = 9.ToString();
+            texts[0, 8].Text = 7.ToString();
+            texts[1, 0].Text = 1.ToString();
+            texts[1, 1].Text = 4.ToString();
+
+            texts[1, 2].Text = 5.ToString();
+            texts[1, 3].Text = 6.ToString();
+            texts[1, 4].Text = 9.ToString();
+            texts[1, 5].Text = 8.ToString();
+            texts[1, 6].Text = 7.ToString();
+            texts[1, 7].Text = 3.ToString();
+
+            texts[1, 8].Text = 2.ToString();
+            texts[2, 0].Text = 5.ToString();
+            texts[2, 1].Text = 1.ToString();
+
+            texts[2, 2].Text = 3.ToString();
+            texts[2, 3].Text = 9.ToString();
+            texts[2, 4].Text = 8.ToString();
+            texts[2, 5].Text = 7.ToString();
+            texts[2, 6].Text = 4.ToString();
+            texts[2, 7].Text = 2.ToString();
+
+            texts[2, 8].Text = 6.ToString();
+            texts[3, 0].Text = 8.ToString();
+            texts[3, 1].Text = 7.ToString();
+
+            texts[3, 2].Text = 6.ToString();
+            texts[3, 3].Text = 5.ToString();
+            texts[3, 4].Text = 2.ToString();
+            texts[3, 5].Text = 9.ToString();
+            texts[3, 6].Text = 1.ToString();
+            texts[3, 7].Text = 4.ToString();
+
+            texts[3, 8].Text = 3.ToString();
+            texts[4, 0].Text = 6.ToString();
+            texts[4, 1].Text = 9.ToString();
+
+            texts[4, 2].Text = 2.ToString();
+            texts[4, 3].Text = 1.ToString();
+            texts[4, 4].Text = 7.ToString();
+            texts[4, 5].Text = 4.ToString();
+            texts[4, 6].Text = 3.ToString();
+            texts[4, 7].Text = 8.ToString();
+
+            texts[4, 8].Text = 5.ToString();
+            texts[5, 0].Text = 9.ToString();
+            texts[5, 1].Text = 6.ToString();
+
+            texts[5, 2].Text = 7.ToString();
+            texts[5, 3].Text = 4.ToString();
+            texts[5, 4].Text = 3.ToString();
+            texts[5, 5].Text = 5.ToString();
+            texts[5, 6].Text = 2.ToString();
+            texts[5, 7].Text = 1.ToString();
+
+            texts[5, 8].Text = 8.ToString();
+
+            texts[6, 0].Text = 3.ToString();
+            texts[6, 1].Text = 8.ToString();
+
+            texts[6, 2].Text = 9.ToString();
+            texts[6, 3].Text = 2.ToString();
+            texts[6, 4].Text = 4.ToString();
+            texts[6, 5].Text = 6.ToString();
+            texts[6, 6].Text = 5.ToString();
+            texts[6, 7].Text = 7.ToString();
+
+            texts[6, 8].Text = 1.ToString();
+            texts[7, 0].Text = 2.ToString();
+            texts[7, 1].Text = 5.ToString();
+
+            texts[7, 2].Text = 4.ToString();
+            texts[7, 3].Text = 7.ToString();
+            texts[7, 4].Text = 1.ToString();
+            texts[7, 5].Text = 3.ToString();
+            texts[7, 6].Text = 8.ToString();
+            texts[7, 7].Text = 6.ToString();
+
+            texts[7, 8].Text = 9.ToString();
+            texts[8, 0].Text = 7.ToString();
+            texts[8, 1].Text = 3.ToString();
+
+            texts[8, 2].Text = 1.ToString();
+            texts[8, 3].Text = 8.ToString();
+            texts[8, 4].Text = 6.ToString();
+            texts[8, 5].Text = 2.ToString();
+            texts[8, 6].Text = 9.ToString();
+            texts[8, 7].Text = 5.ToString();
+
+            texts[8, 8].Text = 4.ToString();
+        }
     }
 }
+
