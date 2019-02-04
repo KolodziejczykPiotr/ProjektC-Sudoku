9297b321f1cd9b2683143310267e8239a2ab8625
Cebul23 <piotr.kolodziejczyk@microsoft.wsei.edu.pl>
2019-02-04 20:39:54 +0100

    Refactoring-code-and-add-Xml-description-and-add-test

---
+++ SudokuEngineTests/SudokuTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SudokuLibrary;
+namespace SudokuEngineTests
+{
+    [TestClass]
+    public class SudokuTests
+    {
+        [TestMethod]
+        public void NewGameTest()
+        {
+            int[,] testBoard = new int[9, 9];
+            int[] testBoard1 = new int[9];
+            bool flag=true;
+            SudokuEngine.NewGame(testBoard,testBoard1);
+            foreach(var i in testBoard)
+            {
+                if (i != 0)
+                {
+                    flag = false;
+                    break;
+                }
+                else
+                {
+                    flag = true;
+                }
+            }
+            Assert.IsFalse(flag);
+
+        }
+        
+    }
+}
